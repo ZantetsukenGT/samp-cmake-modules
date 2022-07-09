84718e06db15de1b8cfc2bad507d3b3492b61b15
Ozmar Escobar <25725331+ZantetsukenGT@users.noreply.github.com>
2022-07-08 22:12:27 -0600

    Update AddSAMPPlugin.cmake

--- AddSAMPPlugin.cmake
+++ AddSAMPPlugin.cmake
@@ -1,31 +1,41 @@
-function(add_samp_plugin plugin_name)
-  add_library(${plugin_name} MODULE ${ARGN})
+function(add_samp_plugin name)
+  add_library(${name} MODULE ${ARGN})
 
-  foreach(file IN ITEMS ${ARGN})
-    get_filename_component(file_path "${file}" PATH)
-
-    string(REPLACE "${CMAKE_SOURCE_DIR}" "" group_path "${file_path}")
-
-    string(REPLACE "/" "\\" group_path "${group_path}")
-
-    source_group("${group_path}" FILES "${file}")
-  endforeach()
-
+  set_target_properties(${name} PROPERTIES PREFIX "")
   target_compile_features(${plugin_name} PRIVATE cxx_std_20)
 
-  set_target_properties(${plugin_name} PROPERTIES PREFIX "")
-
-  if(MSVC)
-    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "WIN32")
-    target_compile_definitions(${plugin_name} PRIVATE _CRT_SECURE_NO_WARNINGS HAVE_STDINT_H)
-
-    target_compile_options(${plugin_name} PRIVATE /wd4305 $<$<CONFIG:>:/MT> $<$<CONFIG:Debug>:/MTd> $<$<CONFIG:Release>:/MT>)
-  elseif(CMAKE_COMPILER_IS_GNUCC)
-    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "LINUX")
+  if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     target_compile_definitions(${plugin_name} PRIVATE _GLIBCXX_USE_CXX11_ABI=0)
 
     set_property(TARGET ${plugin_name} PROPERTY POSITION_INDEPENDENT_CODE Off)
     set_property(TARGET ${plugin_name} APPEND_STRING PROPERTY COMPILE_FLAGS "-m32 -O3 -Wall -Wextra")
     set_property(TARGET ${plugin_name} APPEND_STRING PROPERTY LINK_FLAGS "-m32 -O3 -static-libgcc -static-libstdc++")
+  endif()
+
+  if(CMAKE_COMPILER_IS_GNUCXX)
+    set_property(TARGET ${name} APPEND_STRING PROPERTY
+                 COMPILE_FLAGS " -Wno-attributes")
+  endif()
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set_property(TARGET ${name} APPEND_STRING PROPERTY
+                 COMPILE_FLAGS " -Wno-ignored-attributes")
+  endif()
+
+  if(WIN32 AND CMAKE_COMPILER_IS_GNUCC)
+    set_property(TARGET ${name} APPEND_STRING PROPERTY
+                 LINK_FLAGS " -Wl,--enable-stdcall-fixup")
+  endif()
+
+  if(CYGWIN)
+    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "WIN32")
+    set_property(TARGET ${name} APPEND_STRING PROPERTY LINK_FLAGS " -Wl,--kill-at")
+  elseif(UNIX AND NOT WIN32 AND NOT APPLE)
+    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "LINUX")
+  endif()
+
+  if(MINGW)
+    # Work around missing #include <stddef.h> in <SDK>/amx/amx.h.
+    set_property(TARGET ${name} APPEND_STRING PROPERTY COMPILE_FLAGS " -include stddef.h")
   endif()
 endfunction()
