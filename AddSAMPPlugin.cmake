f75b993084b59e852cb68e5aac4692c8fe563aef
Ozmar Escobar <25725331+ZantetsukenGT@users.noreply.github.com>
2022-07-08 21:54:42 -0600

    Update AddSAMPPlugin.cmake
    * Add compile time definition..
    * Add warnings
    * bump to c++20

--- AddSAMPPlugin.cmake
+++ AddSAMPPlugin.cmake
@@ -11,19 +11,21 @@ function(add_samp_plugin plugin_name)
     source_group("${group_path}" FILES "${file}")
   endforeach()
 
-  target_compile_features(${plugin_name} PRIVATE cxx_std_17)
+  target_compile_features(${plugin_name} PRIVATE cxx_std_20)
 
   set_target_properties(${plugin_name} PROPERTIES PREFIX "")
 
   if(MSVC)
+    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "WIN32")
     target_compile_definitions(${plugin_name} PRIVATE _CRT_SECURE_NO_WARNINGS HAVE_STDINT_H)
 
     target_compile_options(${plugin_name} PRIVATE /wd4305 $<$<CONFIG:>:/MT> $<$<CONFIG:Debug>:/MTd> $<$<CONFIG:Release>:/MT>)
   elseif(CMAKE_COMPILER_IS_GNUCC)
+    set_property(TARGET ${name} APPEND PROPERTY COMPILE_DEFINITIONS "LINUX")
     target_compile_definitions(${plugin_name} PRIVATE _GLIBCXX_USE_CXX11_ABI=0)
 
     set_property(TARGET ${plugin_name} PROPERTY POSITION_INDEPENDENT_CODE Off)
-    set_property(TARGET ${plugin_name} APPEND_STRING PROPERTY COMPILE_FLAGS "-m32 -O3 -w")
+    set_property(TARGET ${plugin_name} APPEND_STRING PROPERTY COMPILE_FLAGS "-m32 -O3 -Wall -Wextra")
     set_property(TARGET ${plugin_name} APPEND_STRING PROPERTY LINK_FLAGS "-m32 -O3 -static-libgcc -static-libstdc++")
   endif()
 endfunction()
